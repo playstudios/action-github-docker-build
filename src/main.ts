b2579e231f3884d5d76980199bd785f99ad1f421
Jon Walton <jonwalton@gmail.com>
2019-12-07 21:18:26 +0800

    initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,46 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+function arg(name: string, value: string): string[] {
+  if (!value) {
+    throw new Error(`Argument '${name}' has an empty value`)
   }
+  return [name, value]
 }
 
-run()
+function argFromEnvArray(name: string, envVars: string[]): string[] {
+  let args: string[] = []
+  for (const e of envVars) {
+    args = args.concat(name, `${e}="${process.env[e] || ''}"`)
+  }
+  return args
+}
+
+async function dockerVersion(): Promise<void> {
+  await exec.exec('docker', ['--version'])
+}
+
+async function dockerBuild(args: string[]): Promise<void> {
+  await exec.exec('docker', ['build', ...args])
+}
+
+async function dockerPush(tag: string): Promise<void> {
+  await exec.exec('docker', ['push', tag])
+}
+
+async function run(): Promise<void> {
+  const tag = [core.getInput('repository'), core.getInput('tag')].join(':')
+
+  await dockerVersion()
+  await dockerBuild([
+    ...arg('-f', core.getInput('dockerfile')),
+    ...arg('-t', tag),
+    ...argFromEnvArray('--build-arg', core.getInput('build_args').split(',')),
+    core.getInput('build_context')
+  ])
+  await dockerPush(tag)
+}
+
+if (process.env.GITHUB_ACTIONS) {
+  run().catch(e => core.setFailed(e.toString()))
+}
